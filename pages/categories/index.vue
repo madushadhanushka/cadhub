b2e15c4acb6c0c760cb6f1b803b111529dffd1bb
dhanushka <madushandhanushka@gmail.com>
2019-10-15 21:38:19 +0530

    fix loading issue

--- pages/categories/index.vue
+++ pages/categories/index.vue
@@ -1,6 +1,12 @@
 <template>
   <div id="categories-page" class="page-wrapper categories-page">
+    <button class="button is-primary" @click="$refs.plan.load()">
+      Subscribe To Newsletter
+    </button>
     <model-obj
+      height="1000"
+      width="1000"
+      ref="plan"
       src="https://cors-anywhere.herokuapp.com/https://firebasestorage.googleapis.com/v0/b/firstbot-a1ca0.appspot.com/o/eyeball.obj?alt=media&token=d2c1a893-c80c-43d1-85b4-b9c5e8a788f7"
       @on-load="onLoad"
     ></model-obj>
@@ -20,6 +26,10 @@ export default {
     onLoad() {
       this.loading = false
     }
+  },
+  mounted() {
+    console.log('Component has been created!')
+    this.$refs.plan.load()
   }
 }
 </script>
